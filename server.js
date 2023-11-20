b58970fe9b8ad3605f08fe16bab0f913ae087d7f
Sanyam-Dhawan-1998 <151523061+Sanyam-Dhawan-1998@users.noreply.github.com>
2023-11-20 18:48:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-connectToDatabase()
-  .then(() => {
-    // MongoDB connected successfully, start the server
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-      console.log("Everything is up and running!");
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error.message);
-  });
+// connectToDatabase();
 
 // Pass the 'app' to setupRoutes function
 setupRoutes(app);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
